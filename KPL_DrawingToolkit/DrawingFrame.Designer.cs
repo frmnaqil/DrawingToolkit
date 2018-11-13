5a8f31aa8fac4b4b2304304859a95f584624d53b
Firman Aqil <frmnaqil@gmail.com>
2018-11-13 10:21:30 +0700

    Initial commit

---
+++ KPL_DrawingToolkit/DrawingFrame.Designer.cs
@@ -0,0 +1,105 @@
+﻿namespace KPL_DrawingToolkit
+{
+    partial class DrawingFrame
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.lineTool = new System.Windows.Forms.ToolStripButton();
+            this.drawingCanvas = new System.Windows.Forms.Panel();
+            this.circleTool = new System.Windows.Forms.ToolStripButton();
+            this.toolStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.GripStyle = System.Windows.Forms.ToolStripGripStyle.Hidden;
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.lineTool,
+            this.circleTool});
+            this.toolStrip1.Location = new System.Drawing.Point(0, 0);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(784, 25);
+            this.toolStrip1.TabIndex = 0;
+            this.toolStrip1.Text = "toolStrip1";
+            // 
+            // lineTool
+            // 
+            this.lineTool.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.lineTool.Image = global::KPL_DrawingToolkit.Properties.Resources.Line;
+            this.lineTool.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.lineTool.Name = "lineTool";
+            this.lineTool.Size = new System.Drawing.Size(23, 22);
+            this.lineTool.Text = "toolStripButton1";
+            this.lineTool.Click += new System.EventHandler(this.lineTool_Click);
+            // 
+            // drawingCanvas
+            // 
+            this.drawingCanvas.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.drawingCanvas.Location = new System.Drawing.Point(0, 28);
+            this.drawingCanvas.Name = "drawingCanvas";
+            this.drawingCanvas.Size = new System.Drawing.Size(784, 533);
+            this.drawingCanvas.TabIndex = 1;
+            this.drawingCanvas.Paint += new System.Windows.Forms.PaintEventHandler(this.drawingCanvas_Paint);
+            this.drawingCanvas.MouseDown += new System.Windows.Forms.MouseEventHandler(this.drawingCanvas_MouseDown);
+            this.drawingCanvas.MouseUp += new System.Windows.Forms.MouseEventHandler(this.drawingCanvas_MouseUp);
+            // 
+            // circleTool
+            // 
+            this.circleTool.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.circleTool.Image = global::KPL_DrawingToolkit.Properties.Resources.Circle;
+            this.circleTool.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.circleTool.Name = "circleTool";
+            this.circleTool.Size = new System.Drawing.Size(23, 22);
+            this.circleTool.Text = "toolStripButton2";
+            this.circleTool.Click += new System.EventHandler(this.circleTool_Click);
+            // 
+            // DrawingFrame
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(784, 561);
+            this.Controls.Add(this.drawingCanvas);
+            this.Controls.Add(this.toolStrip1);
+            this.Name = "DrawingFrame";
+            this.Text = "Simple Drawing Toolkit";
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.ToolStripButton lineTool;
+        private System.Windows.Forms.Panel drawingCanvas;
+        private System.Windows.Forms.ToolStripButton circleTool;
+    }
+}
+
---
+++ KPL_DrawingToolkit/DrawingFrame.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace KPL_DrawingToolkit
+{
+    public partial class DrawingFrame : Form
+    {
+
+        private bool LineToolStatus;
+        private bool CircleToolStatus;
+        private int InitialPointX;
+        private int InitialPointY;
+
+        public DrawingFrame()
+        {
+            InitializeComponent();
+        }
+
+        private void lineTool_Click(object sender, EventArgs e)
+        {
+            LineToolStatus = true;
+            CircleToolStatus = false;
+
+        }
+
+        private void circleTool_Click(object sender, EventArgs e)
+        {
+            LineToolStatus = false;
+            CircleToolStatus = true;
+
+        }
+
+        private void drawingCanvas_MouseDown(object sender, MouseEventArgs e)
+        {
+            InitialPointX = e.X;
+            InitialPointY = e.Y;
+        }
+
+        private void drawingCanvas_MouseUp(object sender, MouseEventArgs e)
+        {
+            if (LineToolStatus == true)
+            {
+                DrawLine(e.X, e.Y);
+            }
+            else DrawCircle(e.X, e.Y);
+        }
+
+        private void DrawCircle(int x, int y)
+        {
+            Pen pen = new Pen(Color.Black, 1);
+            Graphics circle;
+            circle = drawingCanvas.CreateGraphics();
+            circle.DrawEllipse(pen, InitialPointX, InitialPointY, x - InitialPointX, y - InitialPointY);
+
+            pen.Dispose();
+            circle.Dispose();
+        }
+
+        private void DrawLine(int EndPointX, int EndPointY)
+        {
+            Point point1 = new Point(InitialPointX, InitialPointY);
+            Point point2 = new Point(EndPointX, EndPointY);
+
+            Pen pen = new Pen(Color.Black, 1);
+            Graphics line;
+            line = drawingCanvas.CreateGraphics();
+            line.DrawLine(pen, point1, point2);
+
+            pen.Dispose();
+            line.Dispose();
+        }
+
+        private void drawingCanvas_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+    }
+}
