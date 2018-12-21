d5a70715af15ec0b58de870a33092fb999cc8e6e
Unknown <frmnaqil@gmail.com>
2018-12-21 08:52:21 +0700

    Reset UI, delete tools

--- KPL_DrawingToolkit/DrawingFrame.Designer.cs
+++ KPL_DrawingToolkit/DrawingFrame.Designer.cs
@@ -28,112 +28,41 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
-            this.lineTool = new System.Windows.Forms.ToolStripButton();
-            this.circleTool = new System.Windows.Forms.ToolStripButton();
-            this.ToolGradient = new System.Windows.Forms.ToolStripSplitButton();
-            this.color1ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.color2ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.drawingCanvas = new System.Windows.Forms.Panel();
-            this.toolStrip1.SuspendLayout();
+            this.toolStripContainer1 = new System.Windows.Forms.ToolStripContainer();
+            this.toolStripContainer1.SuspendLayout();
             this.SuspendLayout();
             // 
-            // toolStrip1
+            // toolStripContainer1
             // 
-            this.toolStrip1.GripStyle = System.Windows.Forms.ToolStripGripStyle.Hidden;
-            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
-            this.lineTool,
-            this.circleTool,
-            this.ToolGradient});
-            this.toolStrip1.Location = new System.Drawing.Point(0, 0);
-            this.toolStrip1.Name = "toolStrip1";
-            this.toolStrip1.Size = new System.Drawing.Size(784, 25);
-            this.toolStrip1.TabIndex = 0;
-            this.toolStrip1.Text = "toolStrip1";
+            this.toolStripContainer1.BottomToolStripPanelVisible = false;
             // 
-            // lineTool
+            // toolStripContainer1.ContentPanel
             // 
-            this.lineTool.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
-            this.lineTool.Image = global::KPL_DrawingToolkit.Properties.Resources.Line;
-            this.lineTool.ImageTransparentColor = System.Drawing.Color.Magenta;
-            this.lineTool.Name = "lineTool";
-            this.lineTool.Size = new System.Drawing.Size(23, 22);
-            this.lineTool.Text = "toolStripButton1";
-            this.lineTool.Click += new System.EventHandler(this.lineTool_Click);
-            // 
-            // circleTool
-            // 
-            this.circleTool.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
-            this.circleTool.Image = global::KPL_DrawingToolkit.Properties.Resources.Circle;
-            this.circleTool.ImageTransparentColor = System.Drawing.Color.Magenta;
-            this.circleTool.Name = "circleTool";
-            this.circleTool.Size = new System.Drawing.Size(23, 22);
-            this.circleTool.Text = "toolStripButton2";
-            this.circleTool.Click += new System.EventHandler(this.circleTool_Click);
-            // 
-            // ToolGradient
-            // 
-            this.ToolGradient.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
-            this.ToolGradient.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
-            this.color1ToolStripMenuItem,
-            this.color2ToolStripMenuItem});
-            this.ToolGradient.Image = global::KPL_DrawingToolkit.Properties.Resources.Gradient;
-            this.ToolGradient.ImageTransparentColor = System.Drawing.Color.Magenta;
-            this.ToolGradient.Name = "ToolGradient";
-            this.ToolGradient.Size = new System.Drawing.Size(32, 22);
-            this.ToolGradient.Text = "toolStripButton1";
-            this.ToolGradient.Click += new System.EventHandler(this.ToolGradient_Click);
-            // 
-            // color1ToolStripMenuItem
-            // 
-            this.color1ToolStripMenuItem.Name = "color1ToolStripMenuItem";
-            this.color1ToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
-            this.color1ToolStripMenuItem.Text = "Color 1";
-            this.color1ToolStripMenuItem.Click += new System.EventHandler(this.GradientSelectColor1);
-            // 
-            // color2ToolStripMenuItem
-            // 
-            this.color2ToolStripMenuItem.Name = "color2ToolStripMenuItem";
-            this.color2ToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
-            this.color2ToolStripMenuItem.Text = "Color 2";
-            this.color2ToolStripMenuItem.Click += new System.EventHandler(this.GradientSelectColor2);
-            // 
-            // drawingCanvas
-            // 
-            this.drawingCanvas.BackColor = System.Drawing.SystemColors.ControlLightLight;
-            this.drawingCanvas.Location = new System.Drawing.Point(0, 28);
-            this.drawingCanvas.Name = "drawingCanvas";
-            this.drawingCanvas.Size = new System.Drawing.Size(784, 533);
-            this.drawingCanvas.TabIndex = 1;
-            this.drawingCanvas.Paint += new System.Windows.Forms.PaintEventHandler(this.drawingCanvas_Paint);
-            this.drawingCanvas.MouseDown += new System.Windows.Forms.MouseEventHandler(this.drawingCanvas_MouseDown);
-            this.drawingCanvas.MouseUp += new System.Windows.Forms.MouseEventHandler(this.drawingCanvas_MouseUp);
+            this.toolStripContainer1.ContentPanel.Size = new System.Drawing.Size(784, 29);
+            this.toolStripContainer1.LeftToolStripPanelVisible = false;
+            this.toolStripContainer1.Location = new System.Drawing.Point(0, 0);
+            this.toolStripContainer1.Name = "toolStripContainer1";
+            this.toolStripContainer1.RightToolStripPanelVisible = false;
+            this.toolStripContainer1.Size = new System.Drawing.Size(784, 29);
+            this.toolStripContainer1.TabIndex = 2;
+            this.toolStripContainer1.Text = "toolStripContainer1";
             // 
             // DrawingFrame
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(784, 561);
-            this.Controls.Add(this.drawingCanvas);
-            this.Controls.Add(this.toolStrip1);
+            this.Controls.Add(this.toolStripContainer1);
             this.Name = "DrawingFrame";
-            this.Text = "Simple Drawing Toolkit";
-            this.toolStrip1.ResumeLayout(false);
-            this.toolStrip1.PerformLayout();
+            this.Text = "KPL Drawing Toolkit";
+            this.toolStripContainer1.ResumeLayout(false);
+            this.toolStripContainer1.PerformLayout();
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
         #endregion
-
-        private System.Windows.Forms.ToolStrip toolStrip1;
-        private System.Windows.Forms.ToolStripButton lineTool;
-        private System.Windows.Forms.Panel drawingCanvas;
-        private System.Windows.Forms.ToolStripButton circleTool;
-        private System.Windows.Forms.ToolStripSplitButton ToolGradient;
-        private System.Windows.Forms.ToolStripMenuItem color1ToolStripMenuItem;
-        private System.Windows.Forms.ToolStripMenuItem color2ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripContainer toolStripContainer1;
     }
 }
 
--- KPL_DrawingToolkit/DrawingFrame.cs
+++ KPL_DrawingToolkit/DrawingFrame.cs
@@ -11,11 +11,6 @@ namespace KPL_DrawingToolkit
         private IToolbox toolbox;
         private ICanvas canvas;
 
-        private bool LineToolStatus;
-        private bool CircleToolStatus;
-        private int InitialPointX;
-        private int InitialPointY;
-
         public DrawingFrame()
         {
             InitializeComponent();
@@ -39,89 +34,8 @@ namespace KPL_DrawingToolkit
 
         }
 
-
-        private void lineTool_Click(object sender, EventArgs e)
+        private void Toolbox_ToolSelected(ITool tool)
         {
-            LineToolStatus = true;
-            CircleToolStatus = false;
-
-        }
-
-        private void circleTool_Click(object sender, EventArgs e)
-        {
-            LineToolStatus = false;
-            CircleToolStatus = true;
-
-        }
-
-        private void drawingCanvas_MouseDown(object sender, MouseEventArgs e)
-        {
-            InitialPointX = e.X;
-            InitialPointY = e.Y;
-        }
-
-        private void drawingCanvas_MouseUp(object sender, MouseEventArgs e)
-        {
-            if (LineToolStatus == true)
-            {
-                DrawLine(e.X, e.Y);
-            }
-            else DrawCircle(e.X, e.Y);
-        }
-
-        private void DrawCircle(int x, int y)
-        {
-
-            LinearGradientBrush linGrBrush = new LinearGradientBrush(
-                new Point(0, 10),
-                new Point(200, 10),
-                Color.FromArgb(255, 255, 0, 0),   // Opaque red
-                Color.FromArgb(255, 0, 0, 255));  // Opaque blue
-
-            Pen pen = new Pen(linGrBrush);
-            Graphics circle;
-            circle = drawingCanvas.CreateGraphics();
-            circle.FillEllipse(linGrBrush, InitialPointX, InitialPointY, x - InitialPointX, y - InitialPointY);
-
-            pen.Dispose();
-            circle.Dispose();
-        }
-
-        private void DrawLine(int EndPointX, int EndPointY)
-        {
-            Point point1 = new Point(InitialPointX, InitialPointY);
-            Point point2 = new Point(EndPointX, EndPointY);
-
-            Pen pen = new Pen(Color.Black, 1);
-            Graphics line;
-            line = drawingCanvas.CreateGraphics();
-            line.DrawLine(pen, point1, point2);
-
-            pen.Dispose();
-            line.Dispose();
-        }
-
-        private void drawingCanvas_Paint(object sender, PaintEventArgs e)
-        {
-
-        }
-
-        private void ToolGradient_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void GradientSelectColor1(object sender, EventArgs e)
-        {
-            ColorDialog colorDialog1 = new ColorDialog();
-            colorDialog1.ShowDialog();
-
-        }
-
-        private void GradientSelectColor2(object sender, EventArgs e)
-        {
-            ColorDialog colorDialog2 = new ColorDialog();
-            colorDialog2.ShowDialog();
 
         }
     }
